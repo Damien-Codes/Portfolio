cdba2c224fe7a4314e07229a8fa3e6af0337ff05
Skyll_Box <113237888+Damien-Codes@users.noreply.github.com>
2024-03-11 10:06:26 +0100

    Update Share.js

--- Share.js
+++ Share.js
@@ -2,22 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const shareLink = document.getElementById("shareLink");
 
     shareLink.addEventListener("click", function (event) {
-        event.preventDefault(); // Empêche le lien de suivre le lien normal
+        event.preventDefault();
 
-        const customShareUrl = "https://damien-codes.github.io/New_Portefolio/index.html"; // Remplacez par l'URL spécifique
-
+        const customShareUrl = "https://damien-codes.github.io/Portfolio/index.html";
         const shareTitle = document.title;
 
         if (navigator.share) {
             navigator.share({
                 title: shareTitle,
-                url: customShareUrl // Utilisation de l'URL spécifique
+                url: customShareUrl
             })
             .then(() => console.log("Partage réussi"))
             .catch((error) => console.error("Erreur lors du partage :", error));
         } else {
             console.log("Partage non pris en charge par ce navigateur.");
-            // Vous pouvez afficher un message à l'utilisateur indiquant que le partage n'est pas pris en charge.
         }
     });
 });
