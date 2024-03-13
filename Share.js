b9694a322140f4be461e16a57443ad82757dc1eb
Skyll_Box <113237888+Damien-Codes@users.noreply.github.com>
2024-03-14 00:08:57 +0100

    Add files via upload

--- Share.js
+++ Share.js
@@ -2,20 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const shareLink = document.getElementById("shareLink");
 
     shareLink.addEventListener("click", function (event) {
-        event.preventDefault();
+        event.preventDefault(); // Empêche le lien de suivre le lien normal
 
-        const customShareUrl = "https://damien-codes.github.io/Portfolio";
+        const customShareUrl = "https://damien-codes.github.io/New_Portefolio/index.html"; // Remplacez par l'URL spécifique
+
         const shareTitle = document.title;
 
         if (navigator.share) {
             navigator.share({
                 title: shareTitle,
-                url: customShareUrl
+                url: customShareUrl // Utilisation de l'URL spécifique
             })
             .then(() => console.log("Partage réussi"))
             .catch((error) => console.error("Erreur lors du partage :", error));
         } else {
             console.log("Partage non pris en charge par ce navigateur.");
+            // Vous pouvez afficher un message à l'utilisateur indiquant que le partage n'est pas pris en charge.
         }
     });
 });
